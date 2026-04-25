f186364f9a83427f0a6ba38e2c6c92c67946dbe3
jelen <jelenski.jelen@gmail.com>
2026-04-25 16:14:13 +0200

    zadanie 3

--- src/SmartDevice.java
+++ src/SmartDevice.java
@@ -5,8 +5,12 @@ public class SmartDevice {
     private String macAddress;
     private double firmwareVersion;
 
-    public SmartDevice(){
-
+    private SmartDevice(Builder builder){
+        this.id = builder.id;
+        this.name = builder.name;
+        this.room = builder.room;
+        this.macAddress = builder.macAddress;
+        this.firmwareVersion = builder.firmwareVersion;
     }
 
     public String getId() {
@@ -27,6 +31,38 @@ public class SmartDevice {
 
     public double getFirmwareVersion() {
         return firmwareVersion;
+    }
+
+    public static class Builder{
+        private String id;
+        private String name;
+        private String room = "lazienka";
+        private String macAddress = "12:34:34:62:87:45:87:98:22";
+        private double firmwareVersion = 0.0;
+
+        public Builder(String id, String name){
+            this.id = id;
+            this.name = name;
+        }
+
+        public Builder withRoom(String room){
+            this.room=room;
+            return this;
+        }
+
+        public Builder withmacAddress(String macAddress){
+            this.macAddress = macAddress;
+            return this;
+        }
+
+        public Builder withFirmwareVersion(double firmwareVersion){
+            this.firmwareVersion=firmwareVersion;
+            return this;
+        }
+
+        public SmartDevice build(){
+            return new SmartDevice(this);
+        }
     }
 
 
