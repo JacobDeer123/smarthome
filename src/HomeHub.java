e015d7b77922a779c5cda791e076949ecaa8ed7e
jelen <jelenski.jelen@gmail.com>
2026-04-27 17:40:19 +0200

    zadanie 6

--- src/HomeHub.java
+++ src/HomeHub.java
@@ -12,4 +12,13 @@ public class HomeHub {
     public static HomeHub getInstance() {
         return instance;
     }
+
+    public void registerDevice(SmartDevice device) throws DuplicateDeviceException{
+        for (SmartDevice smartDevice : devices) {
+            if (smartDevice.getMacAddress() == device.getMacAddress() || smartDevice.getId() == device.getId()){
+                throw new DuplicateDeviceException("jest juz takie urzadzenie o takim adresie ip lub MAC");
+            }
+        }
+        devices.add(device);
+    }
 }
