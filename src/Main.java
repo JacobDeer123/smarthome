f12568ea2a783f480c223f5eefec40e52f8afd95
jelen <jelenski.jelen@gmail.com>
2026-06-27 14:25:00 +0200

    dalsza część smartthome

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         try{
-            SmartDevice sm = new SmartDevice.Builder("10", "witam").withmacAddress("10").build();
+            SmartDevice sm = new SmartDevice.Builder("10", "witam1").withmacAddress("10").build();
             System.out.println(sm.getId() + "  " + sm.getRoom() + " " + sm.getMacAddress());
         } catch (Exception e) {
             System.err.println(e.getMessage());
